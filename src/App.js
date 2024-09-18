b6e8b56538f06b7229d042f1561ea676ed2d7469
Anıl Başaran <nlbsrn@gmail.com>
2024-09-19 00:40:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import {useState,useEffect} from "react";
+import WeatherCard from "./WeatherCard.jsx";
+import ErrorMessage from "./ErrorMessage.jsx";
+
+const App = () => {
+
+  const [weather,setWeather] = useState(null);
+  const [error,setError] = useState(null);
+
+  const fetchWeather = async ()=>{
+    const response = await fetch("https://api.react-formula.com/learning-api/demos/error-handling/weather");
+console.log(response)
+const data = await response.json();
+    
+    if(response.status == 200){
+    setWeather(data);
+    setError(null);
+      
+    }else{
+      setError(data);
+      setWeather(null);
+      console.log(data)
+      
+    }
+  };
+
+  useEffect(()=>{
+    fetchWeather();
+  },[])
+  
+  return <div className="min-h-screen flex justify-center items-center" >
+    <div className="w-full max-w-lg flex justify-center items-center" >
+      <button
+        onClick={()=>{
+          fetchWeather();
+        }}
+        className="absolute top-5 left-5 rounded-lg bg-teal-300 p-2"><i className=" fa-solid fa-rotate-right"></i></button>
+      {
+        weather && < WeatherCard weather={weather} />
+      }
+
+      {
+        error && <ErrorMessage message = {error.error_message} />
+      }
     </div>
-  );
-}
+  </div>
+};
 
 export default App;
---
+++ src/ErrorMessage.jsx
@@ -0,0 +1,18 @@
+const ErrorMessage = (props) => {
+    const { message } = props;
+    
+    return (
+      <div className="flex flex-col text-stone-500">
+        <div className="flex flex-col items-center text-2xl text-center">
+          <i className="mr-4 text-4xl text-yellow-400 fa-solid fa-triangle-exclamation"></i>
+          <div>{message}</div>
+        </div>
+        <div className="mt-4 text-lg text-center">
+          Try Refreshing?
+        </div>
+      </div>
+    );
+  };
+  
+  export default ErrorMessage;
+  
